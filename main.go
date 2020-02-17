6714f3ab053fb3f34d9312a9fe5b39efbf798a2c
Xanonymous <trusaidlin@gmail.com>
2020-02-18 02:05:59 +0800

    init finished.

--- main.go
+++ main.go
@@ -2,31 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"strconv"
-	"sync"
 )
 
-var counter int
-var mutex = &sync.Mutex{}
-
-func echoString(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello")
-}
-
-func incrementCounter(w http.ResponseWriter, r *http.Request) {
-	mutex.Lock()
-	counter++
-	fmt.Fprintf(w, strconv.Itoa(counter))
-	mutex.Unlock()
-}
-
 func main() {
-	http.HandleFunc("/", echoString)
-	http.HandleFunc("/inc", incrementCounter)
-	http.HandleFunc("hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
 }
