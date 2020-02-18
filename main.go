b3cb69900e3629636f035f5835575e622962ebb0
Xanonymous <trusaidlin@gmail.com>
2020-02-18 16:21:03 +0800

    show requests

--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello~~~~~~~~")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q]=%q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host=%q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr=%q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+		panic(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q]=%q\n", k, v)
+	}
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":9487", nil)
 }
 
